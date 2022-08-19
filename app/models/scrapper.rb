aab5455a4a27c409359b461f243c1966db26f68e
Yu Fan Zheng <anelectronicfan@Yus-MacBook-Pro.local>
2022-08-19 17:11:01 +1000

    can scrape the first 10 properties in each prefecture in the ieichiba site

--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -17,20 +17,46 @@ class Scrapper
   
       prefectures.each do |prefecture|
         # This loop takes each prefecture node and stores their url into an array
-        url = prefecture.attribute('href').value
+        url = prefecture.attribute('href').value # only stores the href as "/area/location/""
+        url = "https://www.ieichiba.com#{url}" # might need to move this line somewhere else if this becomes too slow
         
         prefecture_urls << url # short for prefecture_urls.push
   
       end
+    
+    
 
+    
+
+     scrape_listing_urls(prefecture_urls)
     
   end
 
-  binding.pry
+  def scrape_listing_urls(prefecture_urls)
+
+    property_list = []
+
+    prefecture_urls.each do |url|
+      html = open(url)
+      doc = Nokogiri::HTML(html)
+
+      properties = doc.css('div.property__list-item')
+      
+      properties.each do |property|
+        property_list << property
+      end
+      
+      
+    end
+    
+    binding.pry
+  end
+
 
     
 end
 
 scrape = Scrapper.new
+scrape.scrape_prefecture_urls
 
 
