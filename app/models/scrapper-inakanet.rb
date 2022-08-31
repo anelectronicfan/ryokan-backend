df245b06c223c5fea121d18724a2f00fa51649fe
Yu Fan Zheng <anelectronicfan@Yus-MacBook-Pro.local>
2022-08-31 16:15:47 +1000

    added pseudo code for inakanet scrapper

--- app/models/scrapper-inakanet.rb
+++ app/models/scrapper-inakanet.rb
@@ -5,12 +5,32 @@ require 'colorize'
 require 'mechanize'
 
 
+# STEP 1:
+# Make a scrapper for the first page
+#   Use mechanize to parse the inakanet details page URL
+#   Use css selectors to loop through every table (excluding the first two) in the body tag
+#   During this loop, fetch property details and construct hashes for each
+#   Push these hashes to a collection
+
+# STEP 2:
+# Use Mechanize to submit the next page form
+
+# STEP 3:
+# Loop step 1 and step 2 until the last page is scrapped
+
+# STEP 4:
+# Return the collection to be seeded
 
 
+agent = Mechanize.new
+html = agent.get('http://www.inakanet.jp/cgi-bin/database/database.cgi')
 
-mechanize = Mechanize.new
-page = mechanize.get('http://www.inakanet.jp/cgi-bin/database/database.cgi')
+input = html.forms[0]
 
-input = page.forms[0]
+input['page'] = 2
 
-puts input
+new_page = input.submit
+
+new_paged = Nokogiri::HTML(new_page)
+
+puts new_paged.css('table')[2].css('td')[0].css('tr')[2].css('td')[1].children.text
